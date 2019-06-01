12bbc0e570c4be5bfdbeb5bb8ec099cf74b73429
caktuts <48790595+caktuts@users.noreply.github.com>
2019-06-01 10:52:19 +0300

    Добавил 1 домашнюю работу по Js

---
+++ Lesson_1/script.js
@@ -0,0 +1,27 @@
+var money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+var appData = {
+    budget: money,
+    timeData: time,
+	expenses: {
+        cost:price
+    },
+	optionalExpenses: {},
+	income: [],
+	
+	savings: false
+};
+
+
+
+var cost = prompt("Введите обязательную статью расходов в этом месяце",""),
+    cost2 = prompt("Введите обязательную статью расходов в этом месяце","");
+var price = prompt("Во сколько это обойдется?",""),
+    price2 = prompt("Во сколько это обойдется?","");
+
+alert(appData.budget/30);
+
+cost = cost2;
+price = price2;
+
